d116a39c62fa5bea1a019c73b33ada6f6edf5941
menyagah <menyagah27@gmail.com>
2023-12-07 13:30:41 +0300

    Add routes and retrieve path info

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,11 +2,30 @@
 
 namespace MartinNyagah\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        $content = '<h1>Hello World web</h1>';
-        return new Response(content: $content, status:  200, headers: []);
+
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector){
+            $routeCollector->addRoute('GET', '/', function (){
+                $content = '<h1>Hello World web</h1>';
+                return new Response( $content);
+            });
+            $routeCollector->addRoute('GET', '/post/{id:\d+}', function ($routeParams){
+                $content = "<h1>This is post {$routeParams['id']}</h1>";
+                return new Response( $content);
+            });
+        });
+
+       $routeInfo = $dispatcher->dispatch(
+           $request->getMethod(),
+           $request->getPathInfo()
+       );
+       [$status, $handler, $vars] = $routeInfo;
+       return $handler($vars);
     }
 }
