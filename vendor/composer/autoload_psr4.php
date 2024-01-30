865dea84d3966f18445aa311048bd22f59c458a5
menyagah <menyagah27@gmail.com>
2024-01-30 11:24:14 +0300

    Add dependencies

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,8 +6,12 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Twig\\' => array($vendorDir . '/twig/twig/src'),
+    'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Symfony\\Component\\VarDumper\\' => array($vendorDir . '/symfony/var-dumper'),
+    'Symfony\\Component\\Dotenv\\' => array($vendorDir . '/symfony/dotenv'),
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
     'MartinNyagah\\Framework\\' => array($vendorDir . '/menyagah/myphp-framework/src'),
     'League\\Container\\' => array($vendorDir . '/league/container/src'),
